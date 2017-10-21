2bdd7ffcb2a34ffa79b291789f89f996e08c7e1e
yuanguofeng <yuanguofeng@dinghaotech.com>
2017-10-22 01:00:26 +0800

    add rongchang

---
+++ rongchang.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>畅借贷款iOSV1.1.1Beta</title>
+<meta content="no-cache" http-equiv="pragma">
+<meta content="no-cache" http-equiv="cache-control">
+<meta content="0" http-equiv="expires">
+<meta content="点击下载畅借贷款" http-equiv="description">
+</head>
+<body>
+<center>
+	<div class="dvtext">
+		<a href="itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/puxitech/GangChengHui/master/Rongchang.plist">
+			<font color="#000000">点击下载畅借贷款V1.1.1Beta版</font>
+		</a>
+	</div>
+</center>
+</body>
+</html>
